0f6c52c62f83ce937ab88cded13a6fea83d4a2e3
GRIGS <GRIGS@DESKTOP-NCHROR7>
2024-03-27 22:24:37 +0200

    Add project files.

---
+++ 04InchesToCentimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chislo = double.Parse(Console.ReadLine());
+
+            double result = chislo * 2.54;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 08PetShop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace _08PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogPackages = int.Parse(Console.ReadLine());
+            int catPackages = int.Parse(Console.ReadLine());
+
+            double dogPackagesTotal = 2.50 * dogPackages;
+            double catPackagesTotal = 4 * catPackages;
+
+            double total = dogPackagesTotal + catPackagesTotal;
+
+            Console.WriteLine($"{total} lv.");
+        }
+    }
+}
